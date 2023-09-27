508b39642947fbe2891f3d3e032199db4ac38655
Fakhrillo <dombit1999@gmail.com>
2023-09-27 10:59:22 +0500

    added total checks

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import *
+from django.db.models import Count, Sum
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,9 +8,17 @@ class BranchSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class WorkerSerializer(serializers.ModelSerializer):
+    branch_name = serializers.ReadOnlyField(source='branch.name')  # Add this field to get the branch name
+    total_checks = serializers.SerializerMethodField() # Add this field to get the total checks
+    total_check_sum = serializers.SerializerMethodField() # Add this field to get the total sum
     class Meta:
         model = Workers
-        fields = '__all__'
+        fields = ['id', 'name', 'phone_number', 'branch', 'branch_name', 'id_tg', 'total_checks', 'total_check_sum']
+
+    def get_total_checks(self, obj):
+        return obj.checks_set.count()  # Count the number of checks related to the worker
+    def get_total_check_sum(self, obj):
+        return obj.checks_set.aggregate(Sum('sum'))['sum__sum']  # Sum the 'sum' field of related checks
 
 class CheksSerializer(serializers.ModelSerializer):
     class Meta:
