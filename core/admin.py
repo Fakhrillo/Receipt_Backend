e24eb517e513765df2a53a4cf6e6f12c6af421f3
Fakhrillo <dombit1999@gmail.com>
2023-10-04 10:56:04 +0500

    typo corrected admin

--- core/admin.py
+++ core/admin.py
@@ -9,10 +9,10 @@ class WorkerAdmin(admin.ModelAdmin):
     list_display_links = ['name', 'branch', ]
 
 class ChecksAdmin(admin.ModelAdmin):
-    list_display = ['check_num', 'sum', 'worker', 'branch', 'date', 'image', 'issubmited',]
+    list_display = ['check_num', 'sum', 'worker', 'branch', 'date', 'image', 'issubmitted',]
 
 class DocsAdmin(admin.ModelAdmin):
-    list_display = ['doc_num', 'worker', 'branch', 'date', 'image', 'issubmited',]
+    list_display = ['doc_num', 'worker', 'branch', 'date', 'image', 'issubmitted',]
 
 
 admin.site.register(Branches, BranchAdmin)
