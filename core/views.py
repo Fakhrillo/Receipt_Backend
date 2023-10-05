e51660d7c68caeee6ef333494cfd8a54d7f138c3
Fakhrillo <dombit1999@gmail.com>
2023-10-05 10:12:46 +0500

    push

--- core/views.py
+++ core/views.py
@@ -379,7 +379,7 @@ class UserMessage(APIView):
     def get(self, request):
         message = request.query_params.get('message', None)
         user_id = request.query_params.get('id', None)
-        bot_token = config('bot_token')
+        bot_token = '6642459129:AAGfTt40wSnX6wJFgcC9iwsPSfOAhWLf_Do'
         url = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={user_id}&text={message}'
         response = requests.get(url)
         if response.status_code == 200:
