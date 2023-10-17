c70351c1c20cf837ec739130ad4424d8ee7af7de
Fakhrillo <dombit1999@gmail.com>
2023-10-17 16:18:00 +0500

    new filter

--- core/admin.py
+++ core/admin.py
@@ -7,13 +7,15 @@ class BranchAdmin(admin.ModelAdmin):
 class WorkerAdmin(admin.ModelAdmin):
     list_display = ['name', 'id', 'branch', 'phone_number', 'id_tg',]
     list_display_links = ['name', 'branch', ]
+    list_filter = ('branch')
 
 class ChecksAdmin(admin.ModelAdmin):
     list_display = ['check_num', 'sum', 'worker', 'branch', 'date', 'image', 'issubmitted',]
+    list_filter = ('worker')
 
 class DocsAdmin(admin.ModelAdmin):
     list_display = ['doc_num', 'sum', 'worker', 'branch', 'date', 'image', 'issubmitted',]
-
+    list_filter = ('worker')
 
 admin.site.register(Branches, BranchAdmin)
 admin.site.register(Workers, WorkerAdmin)
