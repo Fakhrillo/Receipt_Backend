2f334947123e6fad4954a40c60f5c4112f2c0b45
Fakhrillo <dombit1999@gmail.com>
2023-10-17 18:34:23 +0500

    formattedd int

--- core/views.py
+++ core/views.py
@@ -344,7 +344,7 @@ class WorkersSummaryView(APIView):
                 'total_docs': total_docs,
                 'total_check_sum': total_check_sum,
                 'total_docs_sum':total_doc_sum,
-                'total_sum': total_doc_sum+total_check_sum,
+                'total_sum': int(total_doc_sum+total_check_sum),
             }
             summary_data.append(data)
         
